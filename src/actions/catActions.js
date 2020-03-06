aa1d51ffdc1a2cd59506b3fd1dc4f0d7643e5649
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-03-06 18:08:06 -0500

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,14 @@
 // write and export your action creator function here
+//Thunk alters the behavior of action creator functions, allowing us to return a function that takes in dispatch. Inside this function, we can execute asynchronous code, and, once resolved, we can use dispatch to update our store with the remote data.
+
+
+  export const fetchCats = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_CATS'})
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+      .then(resp => resp.json())
+        .then(json => {
+          dispatch({type: 'ADD_CATS', cats: json.images})
+        })
+      }
+    }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+//2.import Provider from react-redux
+import { Provider } from 'react-redux';
+//2.import createStore from redux- 4. import applyMiddleware from redux
+import { createStore, applyMiddleware } from 'redux';
+//5. import thunk from redux-thunk (included in package.json)
+import thunk from 'redux-thunk';
+//2.import reducer
+import catsReducer from './reducers/catsReducer.js';
 
+//1. Create Store function and pass the reducer into it
+const store = createStore(catsReducer, applyMiddleware(thunk)) //we'll have to write the reducer.
+//pass thunk to applyMiddleware, pass it in as second argument for createStore
+
+//3.wrap the app component with the Provider, pass the provider to the store.
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+    </Provider>,
     document.getElementById('root')
 );
